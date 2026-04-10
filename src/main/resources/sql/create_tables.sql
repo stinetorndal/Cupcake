8b6302e81d4f7fa41415c47d4ad4843f2428e9ea
stinetorndal <cph-sc95@stud.ek.dk>
2026-04-10 13:24:25 +0200

    admin+kunde done, css updated accordingly and sql scripts updated with newest version

--- src/main/resources/sql/create_tables.sql
+++ src/main/resources/sql/create_tables.sql
@@ -14,22 +14,21 @@ CREATE TABLE public.store
 );
 CREATE TABLE public.users
 (
-    users_id   SERIAL PRIMARY KEY,
+    user_id    SERIAL PRIMARY KEY,
     first_name VARCHAR(50)  NOT NULL,
     last_name  VARCHAR(50)  NOT NULL,
-    username   VARCHAR(50)  NOT NULL,
     email      VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(50)  NOT NULL,
     role       VARCHAR(50)  NOT NULL,
     balance    DECIMAL(10, 2) DEFAULT 0.00
 );
-CREATE TABLE public.top
+CREATE TABLE public.toppings
 (
     topping_id SERIAL PRIMARY KEY,
     name       VARCHAR(255)   NOT NULL,
     price      DECIMAL(10, 2) NOT NULL
 );
-CREATE TABLE public.bottom
+CREATE TABLE public.bottoms
 (
     bottom_id SERIAL PRIMARY KEY,
     name      VARCHAR(255)   NOT NULL,
@@ -38,16 +37,16 @@ CREATE TABLE public.bottom
 CREATE TABLE public.orders
 (
     order_id   SERIAL PRIMARY KEY,
-    users_id   INT REFERENCES cupcake.users (users_id),
+    user_id    INT REFERENCES public.users (user_id),
     created_at TIMESTAMP            DEFAULT CURRENT_TIMESTAMP,
     status     VARCHAR(50) NOT NULL DEFAULT 'pending'
 );
 CREATE TABLE public.order_lines
 (
     line_id    SERIAL PRIMARY KEY,
-    order_id   INT REFERENCES cupcake.orders (order_id) ON DELETE CASCADE,
-    topping_id INT REFERENCES cupcake.top (topping_id),
-    bottom_id  INT REFERENCES cupcake.bottom (bottom_id),
+    order_id   INT REFERENCES public.orders (order_id) ON DELETE CASCADE,
+    topping_id INT REFERENCES public.toppings (topping_id),
+    bottom_id  INT REFERENCES public.bottoms (bottom_id),
     quantity   INT            NOT NULL,
     unit_price DECIMAL(10, 2) NOT NULL,
     discount   INT DEFAULT 0
