54d2ac449f63cf3146de9348a2983066eabeb94c
KatarinaKN <katarinak.nielsen@gmail.com>
2026-04-12 16:25:10 +0200

    Cupcake: added class Cupcake with getters and setters, getTotalPrice() and getFullName().

---
+++ src/main/java/app/entities/Cupcake.java
@@ -0,0 +1,36 @@
+package app.entities;
+
+public class Cupcake {
+
+    private Topping topping;
+    private Bottom bottom;
+
+    public Cupcake(Topping topping, Bottom bottom) {
+        this.topping = topping;
+        this.bottom = bottom;
+    }
+
+    public double getTotalPrice() {
+        return topping.getPrice() + bottom.getPrice();
+    }
+
+    public String getFullName() {
+        return "Bund: " + bottom.getName() + ", top: " + topping.getName();
+    }
+
+    public Topping getTopping() {
+        return topping;
+    }
+
+    public void setTopping(Topping topping) {
+        this.topping = topping;
+    }
+
+    public Bottom getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(Bottom bottom) {
+        this.bottom = bottom;
+    }
+}
