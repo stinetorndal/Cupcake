70b8fed369cdcbaf6e7a41923cf4ce318d5fb295
KatarinaKN <katarinak.nielsen@gmail.com>
2026-04-13 10:46:10 +0200

    User: Changed user_id to userId.

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -1,15 +1,15 @@
 package app.entities;
 
 public class User {
-    private int user_id;
+    private int userId;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
     private String role;
 
-    public User(int user_id, String firstName, String lastName, String email, String password, String role) {
-        this.user_id = user_id;
+    public User(int userId, String firstName, String lastName, String email, String password, String role) {
+        this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -17,9 +17,9 @@ public class User {
         this.role = role;
     }
 
-    public int getUser_id() { return user_id; }
+    public int getUserId() { return userId; }
 
-    public void setUser_id(int user_id) { this.user_id = user_id; }
+    public void setUserId(int userId) { this.userId = userId; }
 
     public String getFirstName() { return firstName; }
 
@@ -44,12 +44,12 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-                "user_id=" + user_id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", role='" + role + '\'' +
+                "user id = " + userId +
+                ", first name = '" + firstName + '\'' +
+                ", last name = '" + lastName + '\'' +
+                ", e-mail = '" + email + '\'' +
+                ", password = '" + password + '\'' +
+                ", role = '" + role + '\'' +
                 '}';
     }
 }
