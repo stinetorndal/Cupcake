92f4111d90b7c515cbe16afcd3a6445f4b94bbde
KatarinaKN <katarinak.nielsen@gmail.com>
2026-04-15 14:22:11 +0200

    CupcakeController: added class CupcakeController

---
+++ src/main/java/app/controllers/CupcakeController.java
@@ -0,0 +1,34 @@
+package app.controllers;
+
+import app.app.exceptions.DatabaseException;
+import app.entities.Bottom;
+import app.entities.Topping;
+import app.persistence.ConnectionPool;
+import app.services.CupcakeService;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CupcakeController {
+
+    public void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.get("/bygdinegencupcake", ctx -> showCupcakeComponents(ctx, connectionPool));
+    }
+
+    public void showCupcakeComponents(Context ctx, ConnectionPool connectionPool) {
+        try {
+            List<Topping> allToppings = new ArrayList<>(CupcakeService.getToppings(connectionPool));
+            List<Bottom> allBottoms = new ArrayList<>(CupcakeService.getBottoms(connectionPool));
+
+            ctx.attribute("toppings", allToppings);
+            ctx.attribute("bottoms", allBottoms);
+            ctx.render("bygdinegencupcake.html");
+
+        } catch (DatabaseException e) {
+            ctx.attribute("message", e.getMessage());
+            ctx.render("bygdinegencupcake.html");
+        }
+    }
+}
